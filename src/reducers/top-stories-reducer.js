6b32dda09bcaa17f1c49c1f3b9ffd3c4287b891b
Patty Rose <oteropatty@gmail.com>
2022-11-19 21:57:36 -0800

    pass test

--- src/reducers/top-stories-reducer.js
+++ src/reducers/top-stories-reducer.js
@@ -1,5 +1,15 @@
+import * as c from '../actions/ActionTypes';
+
 const topStoriesReducer = (state, action) => {
   switch (action.type) {
+
+    case c.GET_TOP_STORIES_SUCCESS:
+      return {
+        ...state, 
+        isLoaded: true,
+        topStories: action.topStories
+      };//Our new action returns a new state object: we use JavaScript's spread syntax to make a copy of the state object, and we specify that isLoaded is set to true and the topStories property is set to action.topStories — the payload we've passed into our action.
+
     default:
       throw new Error(`There is no action matching ${action.type}.`);
   }
