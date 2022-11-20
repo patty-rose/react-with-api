3a03df45c029df5e168a3fca90ca86fb9b79c158
Patty Rose <oteropatty@gmail.com>
2022-11-19 21:54:42 -0800

    write and fail get-top-stories-success reducer test

--- src/_tests_/reducers/top-stories-reducer.test.js
+++ src/_tests_/reducers/top-stories-reducer.test.js
@@ -1,6 +1,10 @@
 import topStoriesReducer from '../../reducers/top-stories-reducer';
+import * as c from './../../actions/ActionTypes';
+
 
 describe('topStoriesReducer', () => {
+
+  let action;
 
   const initialState = {
     isLoaded: false,
@@ -14,5 +18,19 @@ describe('topStoriesReducer', () => {
         topStoriesReducer(initialState, {type: null })
       }
     ).toThrowError("There is no action matching null.");
+  });
+
+  test('successfully getting top stories should change isLoaded to true and update topStories', () => {
+    const topStories = "An article";
+    action = {
+      type: c.GET_TOP_STORIES_SUCCESS,
+      topStories
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+        isLoaded: true,
+        topStories: "An article",
+        error: null
+    });
   });
 });
